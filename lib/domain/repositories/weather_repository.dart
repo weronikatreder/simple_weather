55eafe9acd4b4b6d5ffd65f9f84d7f1ee6b38bcc
Weronika Treder <wtreder53@gmail.com>
2022-03-30 21:38:04 +0200

    Add from.json

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:simple_weather/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
@@ -9,16 +11,14 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    final responseData = await _weatherRemoteDataSource.getWeatherData(
+    final json = await _weatherRemoteDataSource.getWeatherData(
       city: city,
     );
 
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
 
-    final name = responseData['location']['name'] as String;
-    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
-    return WeatherModel(city: name, temperature: temperature);
+    return WeatherModel.fromJson(json);
   }
 }
